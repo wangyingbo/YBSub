b8cc340f4557b66090f8e04add50f7afd1614cc4
Leon Lambert <shi406406@qq.com>
2021-08-14 11:13:46 +0800

    Revert "fix:本地构建未删除失效节点"
    This reverts commit 50b2979ec4823a231f93b44cd233f264a355b2f7.

--- src/test/java/me/leon/NodeCrawler.kt
+++ src/test/java/me/leon/NodeCrawler.kt
@@ -14,7 +14,7 @@ class NodeCrawler {
         private var subCount = 0
         private var nodeCount = 0
         val REG_AD =
-            """$customInfo| new\b|Youtube- MiDnight来了|adi\|\d{4} *- *|顺丰资源You[tT]ube[ |]?|@vpnhat|NB云\|-|TG@peekfun|\{彩虹云}|flyxxl赞助|\([^)]{5,}\)|（.*）|节点更新 ?https?://.+|@SSRSUB-|-付费推荐:.+/ssrsub|https://www.mattkaydiary.com|tg@freebaipiao|@github.com/colatiger-|github.com/freefq - """.toRegex()
+            """ new\b|Youtube- MiDnight来了|adi\|\d{4} *- *|顺丰资源You[tT]ube[ |]?|@vpnhat|NB云\|-|TG@peekfun|\{彩虹云}|flyxxl赞助|\([^)]{5,}\)|（.*）|节点更新 ?https?://.+|@SSRSUB-|-付费推荐:.+/ssrsub|https://www.mattkaydiary.com|tg@freebaipiao|@github.com/colatiger-|github.com/freefq - """.toRegex()
         private val REG_AD_REPALCE =
             """翻墙党fanqiangdang.com|海绵云机场 https://fzusrs.xyz|\[free-ss.site]www.kernels.bid|https://gfwservice.xyz|请订阅-KingFu景福@YouTuBe-自动抓取海量免费节点-https://free.kingfu.cf|白嫖机场：fly.xxl123.fun \| """.toRegex()
     }
